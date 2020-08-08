a0b130ce866ad591859abe40e596e64d5e8b72e0
Anastasiya4 <1914.smile@gmail.com>
2020-08-08 13:40:51 +0300

    seishon[C1ction1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './styles.css';
+import itemTimeplate from '../src/timeplates/reseipt-items.hbs';
+import menu from './menu.json';
+
+const menuDesc = document.querySelector('.js-menu');
+
+menuDesc.insertAdjacentHTML('beforeend', itemTimeplate(menu));
