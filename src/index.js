7e3cb0f4d14a6cf93134122c85b8aaaaf140c531
Anastasiya4 <1914.smile@gmail.com>
2020-08-11 13:13:52 +0300

    section4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import itemTimeplate from './timeplates/reseipt-items.hbs';
 import menu from './menu.json';
+import './js/theme.js';
 
 
 const menuDesc = document.querySelector('.js-menu');
